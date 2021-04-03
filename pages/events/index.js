44014a63b2b8a98957557fdae00e854b11456a7d
labadcloyd <abadcloyd@gmail.com>
2021-04-03 18:47:29 +0800

    routing to the event search submit

--- pages/events/index.js
+++ pages/events/index.js
@@ -2,12 +2,19 @@ import {getAllEvents} from '../../dummy-data'
 import EventList from '../../components/events/event-list'
 import EventSearch from '../../components/events/event-search'
 import { Fragment } from 'react';
+import { useRouter } from 'next/router';
 
 export default function EventsPage(){
 	const data = getAllEvents();
+	const router = useRouter();
+
+	function handleSearch(year, month){
+		const searchPath = `/events/${year}/${month}`
+		router.push(searchPath)
+	}
 	return(
 		<Fragment>
-			<EventSearch></EventSearch>
+			<EventSearch onSearch={handleSearch}></EventSearch>
 			<EventList items={data} />
 		</Fragment>
 	)
