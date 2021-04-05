74baf52ef805308ceddc1fd960fc6243427eccce
labadcloyd <abadcloyd@gmail.com>
2021-04-05 09:54:06 +0800

    added filtered events

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,13 +1,40 @@
 import {useRouter} from 'next/router'
+import {getFilteredEvents} from '../../dummy-data'
+import EventList from '../../components/events/event-list'
+import EventSearch from '../../components/events/event-search'
+import { Fragment } from 'react';
+import Button from '../../components/ui/button'
 
 export default function FilteredEventsPage(){
 	const router = useRouter();
-	const query = router.query.slug
-	console.log(router.query)
+	const filteredQuery = router.query.slug;
+	if(!filteredQuery){
+		return(
+			<p className={'center'}>Loading...</p>
+		)
+	}
+	const numYear = parseInt(filteredQuery[0]) 
+	const numMonth = parseInt(filteredQuery[1]) 
+
+	if(isNaN(numYear)||isNaN(numMonth)||numYear>2030||numYear<2021||numMonth>12||numMonth<1){
+		return(
+			<div className={'center'}><p>Invalid filters, please adjust your values</p><Button link={'/events'}>Show All Events</Button></div>
+		)
+	}
+	const events = getFilteredEvents({year:numYear,month:numMonth})
+	if(!events|| events.length===0){
+		return(
+			<div className={'center'}><p >No events found</p><Button link={'/events'}>Show All Events</Button></div>
+		)
+	}
+	function handleSearch(year, month){
+		const searchPath = `/events/${year}/${month}`
+		router.push(searchPath)
+	}
 	return(
-		<div>
-			<h1>{query[0]}</h1>
-			<h1>{query[1]}</h1>
-		</div>
+		<Fragment>
+			<EventSearch onSearch={handleSearch}></EventSearch>
+			<EventList items={events} />
+		</Fragment>
 	)
 }
